c16f1f4037e9d842658e58d013f81c2394253d72
Jaspreet <52412035+Jaspreet24@users.noreply.github.com>
2022-11-28 13:07:25 +0530

    Updating content

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import { MsalProvider } from '@azure/msal-react';import { msalConfig } from './authConfig';import {PublicClientApplication } from '@azure/msal-browser';import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));const msalInstance = new PublicClientApplication(msalConfig);
 root.render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}><App /></MsalProvider>
   </React.StrictMode>
 );
 
